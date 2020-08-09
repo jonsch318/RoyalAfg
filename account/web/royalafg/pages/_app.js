cadde82deb8e3147e87d8ca54f48405bd7e95e95
Restyled.io <commits@restyled.io>
2020-08-09 22:55:42 +0000

    Restyled by clang-format

--- account/web/royalafg/pages/_app.js
+++ account/web/royalafg/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return < Component { ...pageProps }
+  />
 }
 
 export default MyApp
--- account/web/royalafg/pages/index.js
+++ account/web/royalafg/pages/index.js
@@ -32,32 +32,38 @@ export default function Home() {
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+  className = {styles.card} > <h3>Examples & rarr;
+      </h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p><
+      /a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+            href="https:/ / vercel.com / import
+      ? filter = next.js &utm_source = create - next - app &utm_medium =
+                                           default - template &utm_campaign =
+                                               create - next - app "
+  className = {styles.card} > <h3>Deploy & rarr;
+      </h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
-        </div>
-      </main>
+      </a>
+        </div><
+      /main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
+          href="https:/ /
+          vercel.com
+      ? utm_source = create - next - app &utm_medium =
+                         default - template &utm_campaign =
+                             create - next - app "
+  target = "_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className={
+    styles.logo} />
         </a>
       </footer>
     </div>
