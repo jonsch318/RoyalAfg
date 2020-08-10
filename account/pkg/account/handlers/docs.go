e76af7614702c99b8ab4a88a145c58fd47a0d857
Jonas Schneider <jonas@jonas-linux.fritz.box>
2020-08-11 00:50:52 +0200

    Created swagger openapi docs

--- account/pkg/account/handlers/docs.go
+++ account/pkg/account/handlers/docs.go
@@ -1,6 +1,6 @@
-// Package classification of RoyalAfg Account API
+// Package classification RoyalAfg Auth API
 //
-// Documentation for RoyalAfg Account API
+// Documentation for RoyalAfg Auth API
 //
 //	Schemes:http, https
 //	BasePath: /
@@ -10,10 +10,42 @@
 //	License: MIT http://opensource.org/license/MIT
 //
 //	Consumes:
-//	- application/json
+//	- 	application/json
+//	-	application/x-www-form-urlencoded
 //
 //	Produces:
 //	- application/json
 //
+//
 // swagger:meta
 package handlers
+
+// UserResponse represents a user
+// swagger:response UserResponse
+type userResponseWrapper struct {
+	// The user
+	// in: body
+	Body UserDTO
+}
+
+// ErrorResponse is a generic error response
+// swagger:response ErrorResponse
+type errorResponseWrapper struct {
+	// The error
+	// in: body
+	Body ErrorResponse
+}
+
+// ValidationError shows the failed validation requirements.
+// Each form field that has missing requirements is listet under validationErrors
+// swagger:response ValidationErrorResponse
+type validationErrorWrapper struct {
+	// The validation errors
+	// in: body
+	Body ValidationError
+}
+
+// NoContentResponse is an empty object with no content
+// swagger:response NoContentResponse
+type noContentResponseWrapper struct {
+}
