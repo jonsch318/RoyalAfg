635fe69d78ee14ec47f735c442b60b12362ec670
Jonas Schneider <jonas@jonas-linux.fritz.box>
2020-09-22 18:23:29 +0200

    mobile friendly navbar

--- web/royalafg/widgets/header/nav.js
+++ web/royalafg/widgets/header/nav.js
@@ -5,8 +5,8 @@ export default function NavItems() {
     return (
         <div className="md:flex md:h-full w-full">
             <nav className="block md:flex md:flex-auto md:items-center">
-                <a className="nav-item block py-4 px-4 border-gray-300 border-b-2 border-solid" href="/">Home</a>
-                <a className="nav-item block py-4 px-4 border-gray-300 border-b-2 border-solid" href="/about">About</a>
+                <a className="nav-item block py-4 px-4 md:p-0 border-gray-300 border-b-2 border-solid md:border-none" href="/">Home</a>
+                <a className="nav-item block py-4 px-4 md:p-0 md:border-none border-gray-300 border-b-2 border-solid" href="/about">About</a>
             </nav>
             <div className="idnav md:mr-12 md:flex block my-2">
                 <IdNav></IdNav>
