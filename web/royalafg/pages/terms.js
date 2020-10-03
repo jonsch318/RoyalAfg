e38d4027e9becd5a714fe7d6654a2fd937f823e2
Jonas Schneider <jonas@jonas-linux.fritz.box>
2020-10-03 21:26:22 +0200

    added redux pattern and i18n

---
+++ web/royalafg/pages/terms.js
@@ -0,0 +1,7 @@
+export default function TermsPage() {
+    return (
+        <div>
+            <h1>Terms of Use</h1>
+        </div>
+    )
+}
---
+++ web/royalafg/redux/types/authTypes.js
@@ -0,0 +1,4 @@
+export const LOGIN_USER = "LOGIN_USER";
+export const LOGGED_USER_IN = "LOGGED_USER_IN"
+export const GET_USER = "GET_USER";
+export const LOGOUT_USER = "LOGOUT_USER";
