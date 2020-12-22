6f15a5fa5518b2dd961505da053f75abd8e1509b
JohnnyS318 <jonas.max.schneider@gmail.com>
2020-12-22 20:37:50 +0100

    Fixed Redux

--- web/royalafg/pages/_app.js
+++ web/royalafg/pages/_app.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { useStore } from '../redux/store';
+import '../styles/globals.css';
+import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
+
+    const store = useStore(pageProps.initialReduxState);
+
     return (
-        <div className="main-container">
-            <Component {...pageProps} />
-        </div>
+        <Provider store={store}>
+            <div className="main-container">
+                <Component {...pageProps} />
+            </div>
+        </Provider>
     );
 }
 
