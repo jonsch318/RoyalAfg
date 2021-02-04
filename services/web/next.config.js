37f39f72b71abc3bae0470a745ffe9e6fe23f363
JohnnyS318 <jonas.max.schneider@gmail.com>
2021-02-04 23:34:37 +0100

    changed a few deployment configs

--- services/web/next.config.js
+++ services/web/next.config.js
@@ -1,7 +1,12 @@
+const { process } = require("postcss-preset-env");
+
 module.exports = {
     i18n: {
         locales: ["en-US", "de-DE"],
         defaultLocale: "en-US",
         localeDetection: false
+    },
+    publicRuntimeConfig: {
+        processEnv: process.env,
     }
 };
